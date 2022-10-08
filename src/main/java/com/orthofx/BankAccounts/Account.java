9d4d91267462c2b05c63232b0922b3ea4789ba88
Cinta <cintajose22@gmail.com>
2022-10-08 17:55:23 +0530

    first commit

---
+++ src/main/java/com/orthofx/BankAccounts/Account.java
@@ -0,0 +1,20 @@
+package com.orthofx.BankAccounts;
+
+public class Account {
+	private int accNumber;
+	private String accType;
+	
+	public int getAccNumber() {
+		return accNumber;
+	}
+	public void setAccNumber(int accNumber) {
+		this.accNumber = accNumber;
+	}
+	public String getAccType() {
+		return accType;
+	}
+	public void setAccType(String accType) {
+		this.accType = accType;
+	}	
+	
+}
---
+++ src/main/java/com/orthofx/BankAccounts/Person.java
@@ -0,0 +1,26 @@
+package com.orthofx.BankAccounts;
+
+public class Person {
+	private int id;
+	private String name;
+	private Account account;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Account getAccount() {
+		return account;
+	}
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+	
+}
